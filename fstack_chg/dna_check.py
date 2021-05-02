eb099bb7d0fe47fe0f18db24ab18d01c3160563f
lupnox <lupnox@gmail.com>
2021-05-01 21:10:39 -0500

    Initiation commit

---
+++ fstack_chg/dna_check.py
@@ -0,0 +1,51 @@
+import json
+import uuuid
+import logging
+
+
+def _main(data):
+	#validate data input if bad return 400
+
+
+	if(hasMutation(data)):
+		#log, put on dynamo, return 200
+	else:
+		#log, put on dynamo, return 403
+
+def hasMutation(dna):
+	n = len(dna)
+	mcount = 0
+	gna = ['A','C','G','T']
+	for element in gna:		
+		#horizonal lookup
+		for i in range(n-3):
+			for j in range(n):
+				if dna[j][i] == element and dna[j][i+1] == element and dna[j][i+2] == element and dna[j][i+3] == element:
+					mcount += 1
+					if(mcount>1):
+						print('mutant!')
+						return True
+		#vertical lookup
+		for i in range(n):
+			for j in range(n-3):
+				if dna[j][i] == element and dna[j+1][i] == element and dna[j+2][i] == element and dna[j+3][i] == element:
+					mcount += 1
+					if(mcount>1):
+						print('mutant!')
+						return True
+		#positive diagonal lookup
+		for i in range(n-3):
+			for r in range(n-3):
+				if dna[j][i] == element and dna[j+1][i+1] == element and dna[j+2][i+2] == element and dna[j+3][i+3] == element:
+					mcount += 1
+					if(mcount>1):
+						print('mutant!')
+						return True
+		#negative diagonal lookup
+		for i in range(n-3):
+			for r in range(3, n):
+				if dna[j][i] == element and dna[j-1][i+1] == element and dna[j-2][i+2] == element and dna[j-3][i+3] == element:
+					mcount += 1
+					if(mcount>1):
+						print('mutant!')
+						return True
